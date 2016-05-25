4d45252f120be385444e3db53c195d3f5000cee4
Rusty Howell <rhowell@control4.com>
2016-05-24 22:53:42 -0600

    Chapter 6: Anti-aliasing

--- raytracer.py
+++ raytracer.py
@@ -1,4 +1,7 @@
 #!/usr/bin/env python
+import random
+
+from camera import Camera
 from vector3 import Vec3, dot, unit_vector
 from ray import Ray
 from hitable import Sphere, HitableList, HitRecord
@@ -20,6 +23,7 @@ if __name__ == '__main__':
     with open('image.ppm', 'w') as f:
         nx = 200
         ny = 100
+        ns = 100
         f.write('P3\n{} {}\n255\n'.format(nx, ny))
 
         lower_left = Vec3(-2, -1, -1)
@@ -29,15 +33,18 @@ if __name__ == '__main__':
         world = HitableList()
         world.append(Sphere(Vec3(0, 0, -1), 0.5))
         world.append(Sphere(Vec3(0, -100.5, -1), 100))
-
+        cam = Camera()
         for j in xrange(ny-1, -1, -1):
             for i in xrange(nx):
-                u = float(i) / nx
-                v = float(j) / ny
+                col = Vec3(0,0,0)
+                for _ in xrange(ns):
+                    u = float(i + random.random()) / nx
+                    v = float(j + random.random()) / ny
 
-                r = Ray(origin, lower_left + horizontal * u + vertical * v)
-                p = r.point_at_parameter(2.0)
-                col = color(r, world)
+                    r = cam.get_ray(u, v)
+                    p = r.point_at_parameter(2.0)
+                    col += color(r, world)
+                col /= float(ns)
                 ir = int(255.99 * col.r)
                 ig = int(255.99 * col.g)
                 ib = int(255.99 * col.b)
